da81bb3940ce58ed0bd81daacc1ebbb5e8c7282a
Vandit Narain Tyagi <narainvandit.de@gmail.com>
2022-05-30 00:33:42 +0200

    Transition between page change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.scss';
-import { Route, Routes, useLocation } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import { Route, Routes} from 'react-router-dom';
 import Home from './Components/Welcome';
 import Projects from './Components/Projects'
 import Skills from './Components/Skills';
@@ -8,20 +7,15 @@ import About from './Components/About';
 import Contact from './Components/Contact';
 
 function App() {
-  const location = useLocation();
   return (
     <div className="App">
-        <TransitionGroup>
-          <CSSTransition timeout={100} classNames='transition' key={location.key}>
-            <Routes location={location}>
+            <Routes>
               <Route exact path='/' element={<Home />} />
               <Route path='/projects' element={<Projects />} />
               <Route path='/skills' element={<Skills />} />
               <Route path='/about' element={<About />} />
               <Route path='/contact' element={<Contact />} />
             </Routes>
-          </CSSTransition>
-        </TransitionGroup> 
     </div>
   );
 }
